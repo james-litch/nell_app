995854d1dc41a65bd43578c3a8ad88e42863a884
James Litchfield <james.litchfield99@gmail.com>
2020-03-17 18:26:53 +0000

    require params

--- lib/views/auth/auth_view_model.dart
+++ lib/views/auth/auth_view_model.dart
@@ -1,47 +1,67 @@
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:nell/core/base/base_view_model.dart';
 import 'package:nell/core/constants/route_names.dart';
 import 'package:nell/core/locator.dart';
 import 'package:nell/core/services/auth_service.dart';
 import 'package:nell/core/services/dialog_service.dart';
 import 'package:nell/core/services/navigator_service.dart';
-import 'package:nell/views/home/home_view.dart';
 
 class AuthViewModel extends BaseViewModel {
   final AuthService _authService = locator<AuthService>();
   final DialogService _dialogService = locator<DialogService>();
 
-  Future logIn(String email, String password) async {
+  Future logIn({
+    @required String email,
+    @required String password,
+  }) async {
+    setBusy(true);
+
     var body = {
       "query": signInMutation,
       "variables": {
-        "input": {"email": email, "password": password}
+        "input": {
+          "email": email,
+          "password": password,
+        }
       },
     };
 
     var result = await _authService.logIn(json.encode(body));
 
+    setBusy(false);
     if (result is String) {
       await _dialogService.showDialog(
         title: 'Login Failure',
         description: result,
       );
-    } else {
+    }
+    else {
       locator<NavigatorService>().navigateToAndReplace(HomeViewRoute);
     }
   }
 
-  Future signUp(String name, String email, String password) async {
+  Future signUp({
+    @required String name,
+    @required String email,
+    @required String password,
+  }) async {
+    setBusy(true);
+
     var body = {
       "query": signUpMutation,
       "variables": {
-        "input": {"name": name, "email": email, "password": password}
+        "input": {
+          "name": name,
+          "email": email,
+          "password": password,
+        }
       },
     };
 
     var result = await _authService.logIn(json.encode(body));
+    setBusy(false);
 
     if (result is String) {
       await _dialogService.showDialog(
