8a3d55acf2f25413c7cecf0d1ac48e29a984a355
James Litchfield <james.litchfield99@gmail.com>
2020-03-18 21:58:59 +0000

    add menu

--- lib/views/home/home_mobile.dart
+++ lib/views/home/home_mobile.dart
@@ -1,35 +1,59 @@
 part of home_view;
 
-class _HomeMobile extends StatelessWidget {
+class _HomeMobile extends StatefulWidget {
   final HomeViewModel viewModel;
 
   _HomeMobile(this.viewModel);
-// have actions
 
   @override
+  __HomeMobileState createState() => __HomeMobileState();
+}
+
+class __HomeMobileState extends State<_HomeMobile> {
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  PersistentBottomSheetController menuController;
+  @override
   Widget build(BuildContext context) {
-    Widget menuButton = GestureDetector(
-      onTap: () {
-        viewModel.title = 'menu';
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Icon(Icons.menu),
-      ),
+    void showMenu() {
+      widget.viewModel.title = 'menu';
+      menuController = _scaffoldKey.currentState.showBottomSheet(
+        (context) => BottomSheetWidget(
+          primaryColor: Colors.blue,
+          secondaryColor: Colors.white,
+          body: Center(
+            child: Text(
+              'Menu',
+            ),
+          ),
+        ),
+      );
+    }
+
+    void closeMenu() {
+      if (menuController != null) {
+        menuController.close();
+        menuController = null;
+      }
+    }
+
+    void menuTap() {
+      menuController == null ? showMenu() : closeMenu();
+    }
+
+    Widget menuButton = IconButton(
+      icon: Icon(Icons.menu, size: 30),
+      onPressed: () => menuTap(),
     );
-    Widget accountButton = GestureDetector(
-      onTap: () {
-        viewModel.title = 'account';
-        viewModel.onAccount();
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Icon(Icons.settings),
-      ),
+
+    Widget accountButton = IconButton(
+      icon: Icon(Icons.settings),
+      onPressed: () => widget.viewModel.onAccount(),
     );
+
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        title: Text(viewModel.title),
+        title: Text(widget.viewModel.title),
         leading: menuButton,
         actions: <Widget>[accountButton],
         backgroundColor: Colors.blue,
