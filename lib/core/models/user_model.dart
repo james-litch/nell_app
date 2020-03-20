4f3361a730f2077c733d4abd3b32a1d7956d24db
James Litchfield <james.litchfield99@gmail.com>
2020-03-20 20:31:46 +0000

    add basic user model

--- lib/core/models/user_model.dart
+++ lib/core/models/user_model.dart
@@ -1,7 +1,11 @@
+import 'package:nell/core/models/subject_model.dart';
+
 class User {
   String name;
   String email;
   List<Subject> subjects;
+
+  // 
 
   User();
 
@@ -11,28 +15,7 @@ class User {
 
   Map<String, dynamic> toJson() => {
         'name': name,
-        'age': email,
+        'email': email,
       };
 }
 
-class Subject {
-  String id;
-  String name;
-  bool admin;
-
-  Subject({this.id, this.name, this.admin});
-
-  Subject.fromJson(Map<String, dynamic> json)
-      : id = json['subject']['id'],
-        name = json['subject']['name'],
-        admin = json['admin'];
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['subject']['id'] = this.id;
-    data['subject']['name'] = this.name;
-    data['admin'] = this.admin;
-
-    return data;
-  }
-}
