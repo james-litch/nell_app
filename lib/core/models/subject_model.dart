02aa0b2033886ba3231ee0426bad286a0c1c9908
James Litchfield <james.litchfield99@gmail.com>
2020-03-25 15:26:00 +0000

    add subject attributes

--- lib/core/models/subject_model.dart
+++ lib/core/models/subject_model.dart
@@ -1,21 +1,53 @@
-class Subject {
-  String id;
-  String name;
-  bool admin;
+import 'package:nell/core/base/base_model.dart';
 
-  Subject({this.id, this.name, this.admin});
+class Subject extends BaseModel {
+  final String id;
+  final String name;
+  final bool isAdmin;
+  List<Exam>exams;
+  Map<String, String> definitions;
+  List<Question> questions;
+  List<String> feedback;
 
-  Subject.fromJson(Map<String, dynamic> json)
-      : id = json['subject']['id'],
-        name = json['subject']['name'],
-        admin = json['admin'];
+  Subject({this.id, this.name, this.isAdmin});
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['subject']['id'] = this.id;
     data['subject']['name'] = this.name;
-    data['admin'] = this.admin;
+    data['admin'] = this.isAdmin;
 
     return data;
   }
+
+  Subject.fromJson(Map<String, dynamic> json)
+      : id = json['subject']['id'],
+        name = json['subject']['name'],
+        isAdmin = json['admin'];
+
+  @override
+  List<Object> get props => [id, name, isAdmin];
+
+  @override
+  bool get stringify => true;
+}
+
+class Exam {
+  String id;
+  String name;
+  String description;
+  List<Question> question;
+}
+
+class Question {
+  String id;
+  String question;
+  List<Answer> answers;
+  List<String> answeredBy;
+  int correctAnswer;
+}
+
+class Answer {
+  String answer;
+  int totalChosen;
 }
