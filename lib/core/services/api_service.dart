653ad8ac7cf5b94ce35cf8be1559a568384b13fc
James Litchfield <james.litchfield99@gmail.com>
2020-03-25 15:27:26 +0000

    create base query service

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,3 +1,40 @@
-class ApiService {
-  // Map<String, String> getHeaders() {}
+import 'dart:convert';
+
+import 'package:nell/core/base/base_service.dart';
+import 'package:nell/core/services/storage_service.dart';
+import 'package:http/http.dart' as http;
+import 'package:nell/core/constants/api_constants.dart';
+import '../locator.dart';
+
+class ApiService extends BaseService {
+  final StorageService _storageService = locator<StorageService>();
+
+   getHeaders() {
+    var headers = _storageService.tokens.toJson();
+
+    headers.addAll({'Content-Type': 'application/json'});
+
+    return headers;
+  }
+
+  Future persistToken() {
+    // TODO: get tokens from res headers.
+  }
+
+  Future query(String body) async {
+
+    Map<String,String> headers = getHeaders();
+    var response = await http.post(
+      apiEndpoint,
+      headers: headers,
+      body: body,
+    );
+    var resBody = json.decode(response.body);
+
+    if (resBody['errors'] != null) return (resBody['errors'][0]['message']);
+
+    // persist token
+
+    return resBody;
+  }
 }
