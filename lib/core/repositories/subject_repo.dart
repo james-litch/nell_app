cf9638a895dfdc556a5f09d8e8d57050a6506c6f
James Litchfield <james.litchfield99@gmail.com>
2020-03-25 15:27:05 +0000

    add subject repo

---
+++ lib/core/repositories/subject_repo.dart
@@ -0,0 +1,44 @@
+// get data from api or shared prefefences
+
+import 'dart:convert';
+
+import 'package:nell/core/locator.dart';
+import 'package:nell/core/models/subject_model.dart';
+import 'package:nell/core/services/api_service.dart';
+import 'package:nell/core/services/dialog_service.dart';
+
+class SubjectRepo {
+  ApiService _apiService = locator<ApiService>();
+  DialogService _dialogService = locator<DialogService>();
+
+  Future<List> getSubjects() async {
+    var body = {
+      "query": subjectsQuery,
+      "variables": {},
+    };
+
+    var res = await _apiService.query(json.encode(body));
+
+    if (res['errors'] != null) print(res);
+
+    var subjects = (res['data']['me']['subjects'] as List)
+        .map((subject) => Subject.fromJson(subject))
+        .toList();
+
+    return subjects;
+  }
+
+  String subjectsQuery = '''
+    query {
+      me {
+        subjects {
+          admin
+          subject {
+            name
+            id
+          }
+        }
+      }
+    }
+  ''';
+}
