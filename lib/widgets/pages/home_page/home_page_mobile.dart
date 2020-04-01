3b29505f4daece9548c1bac96c9fa07962a4a09a
James Litchfield <james.litchfield99@gmail.com>
2020-04-01 19:27:43 +0100

    make questions page widget

--- lib/widgets/pages/home_page/home_page_mobile.dart
+++ lib/widgets/pages/home_page/home_page_mobile.dart
@@ -1,10 +1,40 @@
 part of home_page_widget;
 
 class _HomePageMobile extends StatelessWidget {
+  final viewModel;
+
+  const _HomePageMobile({@required this.viewModel});
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('home_page_mobile'),
-    );
+    ThemeData theme = Theme.of(context);
+
+    Widget currentQuestions = viewModel.currentSubject == null
+        ? Center(child: Text('no subject selected'))
+        : ListView.builder(
+            itemCount: viewModel.currentSubject.currentQuestions.length,
+            itemBuilder: (context, int index) {
+              return QuestionCardWidget(
+                question: viewModel.currentSubject.currentQuestions[index],
+                onTap: () {},
+              );
+            },
+          );
+
+    return BasePageWidget(
+        pageName: 'Home',
+        content: Column(
+          children: <Widget>[
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Current Questions',
+                style: theme.textTheme.headline3,
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(child: currentQuestions),
+          ],
+        ));
   }
 }
--- lib/widgets/pages/home_page/home_page_widget.dart
+++ lib/widgets/pages/home_page/home_page_widget.dart
@@ -1,19 +1,21 @@
 library home_page_widget;
 
+import 'package:nell/widgets/question_card/question_card_widget.dart';
+import 'package:nell/widgets/shared/base_page/base_page_widget.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:flutter/material.dart';
 
 part 'home_page_mobile.dart';
-part 'home_page_tablet.dart';
-part 'home_page_desktop.dart';
 
 class HomePageWidget extends StatelessWidget {
+  final viewModel;
+
+  const HomePageWidget({@required this.viewModel});
+
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
-        mobile: _HomePageMobile(),
-        desktop: _HomePageDesktop(),
-        tablet: _HomePageTablet(),
+      mobile: _HomePageMobile(viewModel: viewModel),
     );
   }
 }
