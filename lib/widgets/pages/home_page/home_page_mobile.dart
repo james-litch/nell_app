a1c9512732ec0ac3ee7283460ffd9a7f4ea9e220
James Litchfield <james.litchfield99@gmail.com>
2020-04-01 21:33:46 +0100

    add current and normal questions

--- lib/widgets/pages/home_page/home_page_mobile.dart
+++ lib/widgets/pages/home_page/home_page_mobile.dart
@@ -8,8 +8,22 @@ class _HomePageMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
 
-    Widget currentQuestions = viewModel.currentSubject == null
-        ? Center(child: Text('no subject selected'))
+    Widget currentQuestions = viewModel.currentSubject == null ||
+            viewModel.currentSubject.currentQuestions.length == 0
+        ? Center(child: Text('no current questions'))
+        : ListView.builder(
+            itemCount: viewModel.currentSubject.currentQuestions.length,
+            itemBuilder: (context, int index) {
+              return QuestionCardWidget(
+                question: viewModel.currentSubject.currentQuestions[index],
+                onTap: () {},
+              );
+            },
+          );
+
+    Widget questionsList = viewModel.currentSubject == null ||
+            viewModel.currentSubject.currentQuestions.length == 0
+        ? Center(child: Text('no questions'))
         : ListView.builder(
             itemCount: viewModel.currentSubject.currentQuestions.length,
             itemBuilder: (context, int index) {
@@ -27,13 +41,16 @@ class _HomePageMobile extends StatelessWidget {
             SizedBox(height: 20),
             Align(
               alignment: Alignment.centerLeft,
-              child: Text(
-                'Current Questions',
-                style: theme.textTheme.headline3,
-              ),
+              child:
+                  Text('Current Questions', style: theme.textTheme.headline3),
             ),
-            SizedBox(height: 20),
             Expanded(child: currentQuestions),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Questions', style: theme.textTheme.headline3),
+            ),
+            Expanded(child: questionsList),
           ],
         ));
   }
