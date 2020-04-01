4330be2cba61ef01bcba23b7e53f02525c904770
James Litchfield <james.litchfield99@gmail.com>
2020-04-01 15:09:07 +0100

    add feedback and exams

--- lib/core/models/subject_model.dart
+++ lib/core/models/subject_model.dart
@@ -1,14 +1,14 @@
 import 'package:nell/core/base/base_model.dart';
 
-class Subject extends BaseModel{
+class Subject extends BaseModel {
   final String id;
   final String name;
   final bool isAdmin;
   final List<CurrentQuestion> currentQuestions;
   final List<Definition> dictionary;
-  // final List<Exam>exams;
+  final List<Exam> exams;
   // final List<Question> questions;
-  // final List<String> feedback;
+  final List<String> feedback;
 
   Subject({
     this.dictionary,
@@ -16,6 +16,8 @@ class Subject extends BaseModel{
     this.name,
     this.isAdmin,
     this.currentQuestions,
+    this.exams,
+    this.feedback,
   });
 
   // Map<String, dynamic> toJson() {
@@ -37,17 +39,26 @@ class Subject extends BaseModel{
         .map((i) => Definition.fromJson(i))
         .toList();
 
+    List<Exam> examList = (json['subject']['exams'] as List)
+        .map((i) => Exam.fromJson(i))
+        .toList();
+
+    List<String> feedbackList = (json['subject']['feedback']).cast<String>();
+
     return Subject(
       isAdmin: json['admin'],
       id: json['subject']['id'],
       name: json['subject']['name'],
       currentQuestions: currentQList,
       dictionary: defList,
+      exams: examList,
+      feedback: feedbackList,
     );
   }
 
   @override
-  List<Object> get props => [id, name, isAdmin, currentQuestions, dictionary];
+  List<Object> get props =>
+      [id, name, isAdmin, currentQuestions, dictionary, exams, feedback];
 
   @override
   bool get stringify => true;
@@ -57,7 +68,16 @@ class Exam {
   String id;
   String name;
   String description;
-  List<CurrentQuestion> question;
+
+  Exam({this.id, this.name, this.description});
+
+  factory Exam.fromJson(Map<String, dynamic> json) {
+    return Exam(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+    );
+  }
 }
 
 class CurrentQuestion {
