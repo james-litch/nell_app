2bde927e5f499348c4c1489b660cf7e91092e2ad
James Litchfield <james.litchfield99@gmail.com>
2020-04-01 15:09:23 +0100

    add feedback and exams to query

--- lib/core/repositories/subject_repo.dart
+++ lib/core/repositories/subject_repo.dart
@@ -5,45 +5,62 @@ import 'dart:convert';
 import 'package:nell/core/locator.dart';
 import 'package:nell/core/models/subject_model.dart';
 import 'package:nell/core/services/api_service.dart';
-// import 'package:nell/core/services/dialog_service.dart';
+import 'package:nell/core/services/dialog_service.dart';
 
 class SubjectRepo {
   ApiService _apiService = locator<ApiService>();
-  // DialogService _dialogService = locator<DialogService>();
+  DialogService _dialogService = locator<DialogService>();
 
   Future<List> getSubjects() async {
     var body = {
       "query": subjectsQuery,
     };
 
-    var res = await _apiService.query(json.encode(body));
+    var subjects;
 
-    if(res is String) print(res);
+    try {
+      var res = await _apiService.query(json.encode(body));
 
-    var subjects = (res['data']['me']['subjects'] as List)
-        .map((subject) => Subject.fromJson(subject))
-        .toList();
-
+      subjects = (res['data']['me']['subjects'] as List)
+          .map((subject) => Subject.fromJson(subject))
+          .toList();
+    } catch (e) {
+      _dialogService.showDialog(
+          title: 'Query Error', description: e.toString());
+    }
     return subjects;
   }
 
   String subjectsQuery = '''
     query {
+
       me {
+
         subjects {
           admin
+          
           subject {
             name
             id
+
             currentQuestions {
               question
               id
             }
+
             dictionary{
               id
               phrase
               definition
             }
+
+            exams{
+              id
+              name
+              description
+            }
+
+            feedback
           }
         }
       }
