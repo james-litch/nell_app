eeb1efe6329135583605bb0664498e648c222439
James Litchfield <james.litchfield99@gmail.com>
2020-04-01 21:31:43 +0100

    use factory for user model

--- lib/core/models/user_model.dart
+++ lib/core/models/user_model.dart
@@ -1,16 +1,21 @@
 class User {
+  String id;
   String name;
   String email;
 
   // Tokens tokens
 
-  User();
+  User({this.id, this.name, this.email});
 
-  User.fromJson(Map<String, dynamic> json)
-      : name = json['name'],
-        email = json['email'];
-
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+    );
+  }
   Map<String, dynamic> toJson() => {
+        'id': id,
         'name': name,
         'email': email,
       };
