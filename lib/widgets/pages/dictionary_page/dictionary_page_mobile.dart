0be12622dc554f93cd9d8c0acca87ce2a3667f71
James Litchfield <james.litchfield99@gmail.com>
2020-04-01 19:26:53 +0100

    make dictionary page widget

--- lib/widgets/pages/dictionary_page/dictionary_page_mobile.dart
+++ lib/widgets/pages/dictionary_page/dictionary_page_mobile.dart
@@ -1,10 +1,44 @@
 part of dictionary_page_widget;
 
 class _DictionaryPageMobile extends StatelessWidget {
+  final viewModel;
+
+  const _DictionaryPageMobile({this.viewModel});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('dictionary_page_mobile'),
+    TextEditingController dictionarySerch = TextEditingController();
+    Widget searchBar = RoundedTextBoxWidget(
+      controller: dictionarySerch,
+      isPassword: false,
+      label: 'Search',
+      primaryColor: Colors.white,
+      secondaryColor: Colors.blue,
+      boarder: true,
+      icon: Icons.search,
+    );
+
+    Widget dictionaryDefs = viewModel.currentSubject == null
+        ? Center(child: Text('no subject selected'))
+        : ListView.builder(
+            itemCount: viewModel.currentSubject.dictionary.length,
+            itemBuilder: (context, int index) {
+              return DefinitionCardWidget(
+                definition: viewModel.currentSubject.dictionary[index],
+                onTap: () {},
+              );
+            });
+
+    return BasePageWidget(
+      pageName: 'Dictionary',
+      content: Column(
+        children: <Widget>[
+          SizedBox(height: 20),
+          searchBar,
+          SizedBox(height: 20),
+          Expanded(child: dictionaryDefs)
+        ],
+      ),
     );
   }
 }
--- lib/widgets/pages/dictionary_page/dictionary_page_widget.dart
+++ lib/widgets/pages/dictionary_page/dictionary_page_widget.dart
@@ -1,19 +1,22 @@
 library dictionary_page_widget;
 
+import 'package:nell/widgets/definition_card/definition_card_widget.dart';
+import 'package:nell/widgets/shared/base_page/base_page_widget.dart';
+import 'package:nell/widgets/shared/rounded_text_box/rounded_text_box_widget.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:flutter/material.dart';
 
 part 'dictionary_page_mobile.dart';
-part 'dictionary_page_tablet.dart';
-part 'dictionary_page_desktop.dart';
 
 class DictionaryPageWidget extends StatelessWidget {
+  final viewModel;
+
+  const DictionaryPageWidget({this.viewModel});
+
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
-        mobile: _DictionaryPageMobile(),
-        desktop: _DictionaryPageDesktop(),
-        tablet: _DictionaryPageTablet(),
+      mobile: _DictionaryPageMobile(viewModel: viewModel),
     );
   }
 }
