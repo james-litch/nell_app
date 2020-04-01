561ffcbc6cc07c35f22278ea30e582abaebbeee1
James Litchfield <james.litchfield99@gmail.com>
2020-04-01 19:27:22 +0100

    make exam page widget

--- lib/widgets/pages/exam_page/exam_page_mobile.dart
+++ lib/widgets/pages/exam_page/exam_page_mobile.dart
@@ -1,10 +1,27 @@
 part of exam_page_widget;
 
 class _ExamPageMobile extends StatelessWidget {
+  final viewModel;
+
+  const _ExamPageMobile({this.viewModel});
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('exam_page_mobile'),
+    Widget examCards = viewModel.currentSubject == null
+        ? Center(child: Text('no subject selected'))
+        : ListView.builder(
+            itemCount: viewModel.currentSubject.exams.length,
+            itemBuilder: (context, int index) {
+              return ExamCardWidget(
+                exam: viewModel.currentSubject.exams[index],
+                onTap: () {},
+              );
+            });
+
+    return BasePageWidget(
+      pageName: 'Exams',
+      content: Column(
+        children: <Widget>[SizedBox(height: 20), Expanded(child: examCards)],
+      ),
     );
   }
 }
--- lib/widgets/pages/exam_page/exam_page_widget.dart
+++ lib/widgets/pages/exam_page/exam_page_widget.dart
@@ -1,19 +1,20 @@
 library exam_page_widget;
 
+import 'package:nell/widgets/exam_card/exam_card_widget.dart';
+import 'package:nell/widgets/shared/base_page/base_page_widget.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:flutter/material.dart';
 
 part 'exam_page_mobile.dart';
-part 'exam_page_tablet.dart';
-part 'exam_page_desktop.dart';
 
 class ExamPageWidget extends StatelessWidget {
+  final viewModel;
+
+  const ExamPageWidget({this.viewModel});
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
-        mobile: _ExamPageMobile(),
-        desktop: _ExamPageDesktop(),
-        tablet: _ExamPageTablet(),
+      mobile: _ExamPageMobile(viewModel: viewModel),
     );
   }
 }
