de010f161098f9278eb62a1d3f3965a0a33d66a3
James Litchfield <james.litchfield99@gmail.com>
2020-04-03 13:17:59 +0100

    add stub methods for getting questions

--- lib/core/repositories/subject_repo.dart
+++ lib/core/repositories/subject_repo.dart
@@ -2,7 +2,9 @@
 
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:nell/core/locator.dart';
+import 'package:nell/core/models/question_model.dart';
 import 'package:nell/core/models/subject_model.dart';
 import 'package:nell/core/services/api_service.dart';
 import 'package:nell/core/services/dialog_service.dart';
@@ -30,6 +32,47 @@ class SubjectRepo {
     }
     return subjects;
   }
+
+  Future<Question> getQuestion({
+    @required String subjectId,
+    @required String questionId,
+  }) async {
+    var body = {
+      "query": questionQuery,
+      "input": {
+        "subjectId": subjectId,
+        "questionId": questionId,
+      }
+    };
+
+    var res = await _apiService.query(json.encode(body));
+
+    var question = Question.fromJson(res['data']['findQuestion']);
+
+    return question;
+  }
+
+  Future answerQuestion({
+    @required String subjectId,
+    @required String questionId,
+    @required int answer,
+  }) async {
+    var body = {
+      "query": questionQuery,
+      "input": {
+        "subjectId": subjectId,
+        "questionId": questionId,
+        "answer": answer
+      }
+    };
+    var res = await _apiService.query(json.encode(body));
+
+    var question = Question.fromJson(res['data']['findQuestion']);
+
+    return question;
+  }
+
+  String questionQuery = ''' ''';
 
   String subjectsQuery = '''
     query {
