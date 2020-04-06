0deda017dd9b9470c502d523bd583aa22254041e
James Litchfield <james.litchfield99@gmail.com>
2020-04-06 23:24:34 +0100

    allow leaving / deleting subjects

--- lib/widgets/subject_menu_item/subject_menu_item_mobile.dart
+++ lib/widgets/subject_menu_item/subject_menu_item_mobile.dart
@@ -3,19 +3,49 @@ part of subject_menu_item_widget;
 class _SubjectMenuItemMobile extends StatelessWidget {
   final Subject subject;
   final onTap;
+  final onMenuTap;
+  final menuItems;
 
-  const _SubjectMenuItemMobile({@required this.subject, @required this.onTap});
+  const _SubjectMenuItemMobile({
+    @required this.subject,
+    @required this.onTap,
+    @required this.onMenuTap,
+    @required this.menuItems,
+  });
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
-    return ListTile(
-      onTap: onTap,
-      title: Padding(
-        padding: EdgeInsets.all(15),
-        child: Text(
-          '${subject.name}',
-          textAlign: TextAlign.center,
-          style: theme.textTheme.headline2,
+
+    Widget menu = PopupMenuButton(
+      onSelected: (value) => onMenuTap(value),
+      icon: Icon(Icons.more_vert, color: Colors.white),
+      itemBuilder: (BuildContext context) => menuItems,
+    );
+
+    return Card(
+      margin: EdgeInsets.all(10),
+      color: Colors.blue,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Colors.white, width: 2),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(20),
+        onTap: onTap,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: ListTile(
+                contentPadding: EdgeInsets.fromLTRB(50.0, 5.0, 5.0, 5.0),
+                title: Text(
+                  '${subject.name}',
+                  style: theme.textTheme.headline2.copyWith(fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            menu,
+          ],
         ),
       ),
     );
--- lib/widgets/subject_menu_item/subject_menu_item_widget.dart
+++ lib/widgets/subject_menu_item/subject_menu_item_widget.dart
@@ -9,14 +9,23 @@ part 'subject_menu_item_mobile.dart';
 class SubjectMenuItemWidget extends StatelessWidget {
   final Subject subject;
   final onTap;
+  final onMenuTap;
+  final menuItems;
 
-  const SubjectMenuItemWidget({@required this.subject, @required this.onTap});
+  const SubjectMenuItemWidget({
+    @required this.subject,
+    @required this.onTap,
+    @required this.onMenuTap,
+    @required this.menuItems,
+  });
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
       mobile: _SubjectMenuItemMobile(
         subject: subject,
         onTap: onTap,
+        menuItems: menuItems,
+        onMenuTap: onMenuTap,
       ),
     );
   }
