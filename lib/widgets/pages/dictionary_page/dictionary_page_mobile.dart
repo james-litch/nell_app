e2a67b2079e54c711b35cd835a4d20d41bf3550d
James Litchfield <james.litchfield99@gmail.com>
2020-04-06 23:23:34 +0100

    add  add definition

--- lib/widgets/pages/dictionary_page/dictionary_page_mobile.dart
+++ lib/widgets/pages/dictionary_page/dictionary_page_mobile.dart
@@ -2,13 +2,16 @@ part of dictionary_page_widget;
 
 class _DictionaryPageMobile extends StatelessWidget {
   final viewModel;
+  final scaffoldKey;
 
-  const _DictionaryPageMobile({this.viewModel});
+  const _DictionaryPageMobile({this.viewModel, @required this.scaffoldKey});
 
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
     TextEditingController dictionarySerch = TextEditingController();
+    TextEditingController phraseController = TextEditingController();
+    TextEditingController definitionController = TextEditingController();
     Widget searchBar = RoundedTextBoxWidget(
       controller: dictionarySerch,
       isPassword: false,
@@ -19,8 +22,86 @@ class _DictionaryPageMobile extends StatelessWidget {
       icon: Icons.search,
     );
 
+    Widget addDefinitionBody = Container(
+      height: 500,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 20),
+          RoundedTextBoxWidget(
+            controller: phraseController,
+            isPassword: false,
+            label: 'Phrase',
+            icon: Icons.textsms,
+            primaryColor: Colors.white,
+            secondaryColor: Colors.blue,
+            boarder: true,
+          ),
+          SizedBox(height: 20),
+          RoundedTextBoxWidget(
+            controller: definitionController,
+            isPassword: false,
+            label: 'Definition',
+            primaryColor: Colors.white,
+            secondaryColor: Colors.blue,
+            icon: Icons.text_fields,
+            boarder: true,
+          ),
+          SizedBox(height: 20),
+          RoundedButtonWidget(
+              text: 'Submit',
+              primaryColor: Colors.blue,
+              secondaryColor: Colors.white,
+              boarder: true,
+              function: () {
+                viewModel.addDefinition(
+                  phrase: phraseController.text,
+                  definition: definitionController.text,
+                );
+                viewModel.menuOpen = false;
+                definitionController.clear();
+                phraseController.clear();
+                Navigator.of(context).pop();
+                viewModel.showTabs = true;
+              }),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
+
+    void addDefinitionPanel() {
+      viewModel.menuOpen = true;
+      viewModel.showTabs = false;
+
+      scaffoldKey.currentState.showBottomSheet(
+        (context) => BottomSheetWidget(
+          primaryColor: Colors.blue,
+          secondaryColor: Colors.white,
+          sheetTop: Row(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.close, color: Colors.white, size: 30),
+                onPressed: () {
+                  viewModel.menuOpen = false;
+                  definitionController.clear();
+                  phraseController.clear();
+                  Navigator.of(context).pop();
+                  viewModel.showTabs = true;
+                },
+              ),
+              Text(
+                'Add Definition',
+                style: theme.textTheme.headline2,
+              ),
+            ],
+          ),
+          body: Container(child: addDefinitionBody),
+        ),
+      );
+    }
+
     Widget dictionaryDefs = viewModel.currentSubject == null ||
-    viewModel.currentSubject.dictionary.length == 0
+            viewModel.currentSubject.dictionary.length == 0
         ? Center(child: Text('no definitions available'))
         : ListView.builder(
             itemCount: viewModel.currentSubject.dictionary.length,
@@ -48,6 +129,19 @@ class _DictionaryPageMobile extends StatelessWidget {
           SizedBox(height: 20),
           searchBar,
           SizedBox(height: 20),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text('Definitions', style: theme.textTheme.headline3),
+              ),
+              IconButton(
+                icon: Icon(Icons.add, color: Colors.blue),
+                onPressed: () => addDefinitionPanel(),
+              )
+            ],
+          ),
           Expanded(child: dictionaryDefs)
         ],
       ),
--- lib/widgets/pages/dictionary_page/dictionary_page_widget.dart
+++ lib/widgets/pages/dictionary_page/dictionary_page_widget.dart
@@ -2,6 +2,8 @@ library dictionary_page_widget;
 
 import 'package:nell/widgets/definition_card/definition_card_widget.dart';
 import 'package:nell/widgets/shared/base_page/base_page_widget.dart';
+import 'package:nell/widgets/shared/bottom_sheet/bottom_sheet_widget.dart';
+import 'package:nell/widgets/shared/rounded_button/rounded_button_widget.dart';
 import 'package:nell/widgets/shared/rounded_text_box/rounded_text_box_widget.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:flutter/material.dart';
@@ -10,13 +12,17 @@ part 'dictionary_page_mobile.dart';
 
 class DictionaryPageWidget extends StatelessWidget {
   final viewModel;
+  final scaffoldKey;
 
-  const DictionaryPageWidget({this.viewModel});
+  const DictionaryPageWidget({this.viewModel, @required this.scaffoldKey});
 
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
-      mobile: _DictionaryPageMobile(viewModel: viewModel),
+      mobile: _DictionaryPageMobile(
+        viewModel: viewModel,
+        scaffoldKey: scaffoldKey,
+      ),
     );
   }
 }
