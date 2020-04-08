e021e5882a9000ef26018272d915b3f9fedd6231
James Litchfield <james.litchfield99@gmail.com>
2020-04-08 14:26:00 +0100

    add exams if admin

--- lib/widgets/pages/exam_page/exam_page_mobile.dart
+++ lib/widgets/pages/exam_page/exam_page_mobile.dart
@@ -7,8 +7,110 @@ class _ExamPageMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
+
+    void addExamsPanel() {
+      viewModel.menuOpen = true;
+      viewModel.showTabs = false;
+
+      List editors = List.generate(
+        2,
+        (index) => TextEditingController(),
+      );
+
+      List questions = List();
+
+      Scaffold.of(context).showBottomSheet(
+        (context) => BottomSheetWidget(
+          primaryColor: Colors.blue,
+          secondaryColor: Colors.white,
+          sheetTop: Row(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.close, color: Colors.white, size: 30),
+                onPressed: () {
+                  viewModel.menuOpen = false;
+                  Navigator.of(context).pop();
+                  viewModel.showTabs = true;
+                },
+              ),
+              Text(
+                'Add Question',
+                style: theme.textTheme.headline2,
+              ),
+            ],
+          ),
+          body: Container(
+            height: 670,
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RoundedTextBoxWidget(
+                  controller: editors[0],
+                  isPassword: false,
+                  label: 'Exam Name',
+                  primaryColor: Colors.white,
+                  secondaryColor: Colors.blue,
+                  boarder: false,
+                ),
+                SizedBox(height: 20),
+                RoundedTextBoxWidget(
+                  controller: editors[1],
+                  isPassword: false,
+                  label: 'Description',
+                  primaryColor: Colors.white,
+                  secondaryColor: Colors.blue,
+                  boarder: false,
+                ),
+                SizedBox(height: 10),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Select questions to add:',
+                    style:
+                        theme.textTheme.headline3.copyWith(color: Colors.white),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: viewModel.currentSubject.questions.length,
+                    itemBuilder: (context, int index) {
+                      return QuestionCardWidget(
+                        onTapColor: Colors.red,
+                        hasMenu: false,
+                        question: viewModel.currentSubject.questions[index],
+                        onTap: () {
+                          var id = viewModel.currentSubject.questions[index].id;
+                          questions.contains(id)
+                              ? questions.remove(id)
+                              : questions.add(id);
+                        },
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                RoundedButtonWidget(
+                  text: 'Submit',
+                  primaryColor: Colors.blue,
+                  secondaryColor: Colors.white,
+                  boarder: true,
+                  function: () => viewModel.addExam(
+                    name: editors[0].text,
+                    description: editors[1].text,
+                    questions: questions,
+                  ),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
     Widget examCards = viewModel.currentSubject == null ||
-    viewModel.currentSubject.exams.length == 0
+            viewModel.currentSubject.exams.length == 0
         ? Center(child: Text('no exams available'))
         : ListView.builder(
             itemCount: viewModel.currentSubject.exams.length,
@@ -33,7 +135,19 @@ class _ExamPageMobile extends StatelessWidget {
     return BasePageWidget(
       pageName: 'Exams',
       content: Column(
-        children: <Widget>[SizedBox(height: 20), Expanded(child: examCards)],
+        children: <Widget>[
+          SizedBox(height: 20),
+          viewModel.currentSubject.isAdmin
+              ? RoundedButtonWidget(
+                  text: 'Add Exam',
+                  primaryColor: Colors.blue,
+                  secondaryColor: Colors.white,
+                  boarder: false,
+                  function: () => addExamsPanel(),
+                )
+              : Container(),
+          Expanded(child: examCards)
+        ],
       ),
     );
   }
--- lib/widgets/pages/exam_page/exam_page_widget.dart
+++ lib/widgets/pages/exam_page/exam_page_widget.dart
@@ -1,7 +1,11 @@
 library exam_page_widget;
 
 import 'package:nell/widgets/exam_card/exam_card_widget.dart';
+import 'package:nell/widgets/question_card/question_card_widget.dart';
 import 'package:nell/widgets/shared/base_page/base_page_widget.dart';
+import 'package:nell/widgets/shared/bottom_sheet/bottom_sheet_widget.dart';
+import 'package:nell/widgets/shared/rounded_button/rounded_button_widget.dart';
+import 'package:nell/widgets/shared/rounded_text_box/rounded_text_box_widget.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:flutter/material.dart';
 
