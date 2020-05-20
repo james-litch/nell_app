48d099a7cddbd24d145a6dcacffa090bf71aeab0
James Litchfield <james.litchfield99@gmail.com>
2020-05-20 20:07:20 +0100

    removed unused method

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:nell/core/base/base_service.dart';
 import 'package:nell/core/models/token_model.dart';
 import 'package:nell/core/services/storage_service.dart';
-import 'package:http/http.dart' as http;
 import 'package:nell/core/constants/api_constants.dart';
 import '../locator.dart';
 
@@ -14,7 +11,6 @@ class ApiService extends BaseService {
 
   getHeaders() {
     var tokens = _storageService.tokens.toJson();
-
     return {
       "Content-Type": "application/json",
       "access-token": tokens['access-token'],
@@ -24,40 +20,19 @@ class ApiService extends BaseService {
 
   Future persistToken(var headers) async {
     if (headers['access-token'] != null && headers['refresh-token'] != null) {
-      
       var tokens = Tokens(
         headers['access-token'][0],
         headers['refresh-token'][0],
       );
-
       _storageService.tokens = tokens;
     }
-
     return true;
-  }
-
-  Future query2(String body) async {
-    log.i("ApiService: query");
-    var headers = getHeaders();
-    var response = await http.post(
-      apiEndpoint,
-      headers: headers,
-      body: body,
-    );
-    var resBody = json.decode(response.body);
-
-    if (resBody['errors'] != null) return (resBody['errors'][0]['message']);
-
-    // persist token
-
-    return resBody;
   }
 
   Future query(String body) async {
     log.i("ApiService: query");
     var headers = getHeaders();
     var response;
-
     try {
       response = await client.post(
         apiEndpoint,
@@ -70,9 +45,26 @@ class ApiService extends BaseService {
     }
     if (response.data['errors'] != null)
       return (response.data['errors'][0]['message']);
-
-    // persist token
     persistToken(response.headers);
     return response.data;
   }
+
+  // Future query(String body) async {
+  //   log.i("ApiService: query");
+  //   var headers = getHeaders();
+  //   var response = await http.post(
+  //     apiEndpoint,
+  //     headers: headers,
+  //     body: body,
+  //   );
+  //   var resBody = json.decode(response.body);
+
+  //   if (resBody['errors'] != null) return (resBody['errors'][0]['message']);
+
+  //   // persist token
+
+  //   return resBody;
+  // }
+
+  
 }
